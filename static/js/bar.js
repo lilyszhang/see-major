ccfed06f8ea84d2ebb5cd7aabacca8f714b56f51
allisonsun <allisunson@gmail.com>
2017-03-14 12:32:49 -0500

    added d3 visualizations

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,3 +1,15 @@
+//shape changes on each beat (librosa beat tracking)
+//make shape that shows frequency more
+//more contrast in peaks
+//color contrast
+
+//24 by 36
+//mention packages
+//next steps
+//technical implementation
+//screenshots
+
+
 //https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
 $(document).ready(function () {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
@@ -11,8 +23,8 @@ $(document).ready(function () {
 
   var frequencyData = new Uint8Array(50); //where frequency data will be copied into
 
-  var svgHeight = '640';
-  var svgWidth = '1450';
+  var svgHeight = $(window).height();
+  var svgWidth = $(window).width();
   var barPadding = '2';
 
   function createSvg(parent, height, width) {
@@ -37,13 +49,13 @@ $(document).ready(function () {
      svg.selectAll('rect')
         .data(frequencyData)
         .attr('y', function(d) {
-           return svgHeight - d;
+           return 0.5* (svgHeight - d);
         })
         .attr('height', function(d) {
-           return d;
+           return d*1.5;
         })
         .attr('fill', function(d) {
-           return 'rgb(100, 50, ' + d + ')';
+           return 'rgb(200, 50, ' + d + ')';
         });
   }
   getData();
