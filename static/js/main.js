20bc958c0494188cfe8d71834ab5ae25f6f2c844
Lily Zhang <lilyszhang96@gmail.com>
2017-03-15 13:51:39 -0500

    added song selection

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,15 @@ audioElement.loop = true;
 function togglePlay() {
   return audioElement.paused ? audioElement.play() : audioElement.pause();
 };
+
+var audioPicker = document.getElementById("audioSelect");
+function updateSong() {
+  var song = audioPicker.options[audioPicker.selectedIndex].value;
+  if (song == "1") {
+    audioElement.src = "/static/audio/kanye.mp3";
+  }
+  if (song == "2") {
+    audioElement.src = '/static/audio/swift.wav';
+  }
+  audioElement.load();
+}
